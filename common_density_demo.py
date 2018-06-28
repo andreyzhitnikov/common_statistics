bbcbb0604b09dc5b2479afe49107e22424dbb334
andreyzhitnikov <andreyzhitnikov@gmail.com>
2018-06-29 01:37:06 +0300

    updated

--- common_density_demo.py
+++ common_density_demo.py
@@ -1,13 +1,14 @@
 import numpy as np
 from common_statistics import est_common_density2D
 import matplotlib.pyplot as plt
+import scipy.stats
 # ---create views---
 vnum = 16
 num = int(np.floor(5000/vnum)*vnum)
 
 
 def generate_u_triangle(num):
-    r = 15
+    r = 30
     ss = r/2*np.random.random(int(num/3))-r/2
     tt = r/2*np.random.random(int(num/3))
     uu = r*np.random.random(int(num/3))-r/2
@@ -20,8 +21,8 @@ np.random.seed(8409)
 
 
 DeT = 100
-h = 0.7
-cond_num = 2
+h = 0.15
+cond_num = 7
 data = []
 
 for i in range(0, vnum):
@@ -56,7 +57,25 @@ for j in range(0, vnum):
     axarr1[row, col].set_xticks([xmin*2/3, 0, xmax*2/3])
     axarr1[row, col].set_yticks([ymin*2/3, 0, ymax*2/3])
     axarr1[row, col].tick_params(axis='both', which='major', labelsize=10)
+    axarr1[row, col].set_axis_off()
 
+x1, x2 = np.meshgrid(np.linspace(xmin, xmax, num=dimx, endpoint=True, retstep=False, dtype=None), np.linspace(ymin, ymax, num=dimy,endpoint=True))
+pos = np.vstack([x1.ravel(), x2.ravel()])
+values = np.hstack(data)
+kernel = scipy.stats.gaussian_kde(values, bw_method=h)
+pdfAll = np.reshape(kernel.evaluate(pos).T, x1.shape)
+
+u = generate_u_triangle(num)
+kernel2 = scipy.stats.gaussian_kde(u, bw_method=h)
+pdfGround_truth = np.reshape(kernel2.evaluate(pos).T, x1.shape)
+
+fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+axs[0].imshow(pdfGround_truth, origin='lower', interpolation='nearest', cmap=plt.cm.gist_earth_r, extent=[xmin, xmax, ymin, ymax], vmin=None, vmax=None)
+axs[0].set_axis_off()
+axs[1].imshow(pdfAll, origin='lower', interpolation='nearest', cmap=plt.cm.gist_earth_r, extent=[xmin, xmax, ymin, ymax], vmin=None, vmax=None)
+axs[1].set_axis_off()
+axs[2].imshow(pUMatr, origin='lower', interpolation='nearest', cmap=plt.cm.gist_earth_r, extent=[xmin, xmax, ymin, ymax], vmin=None, vmax=None)
+axs[2].set_axis_off()
 plt.show()
 
 
